6b89d00188acbd991113a40f7047f92288a05e19
kandahika <166777356+kandahika@users.noreply.github.com>
2024-06-14 12:33:48 +0900

    omikuji

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from typing import Optional
 
 from fastapi import FastAPI
 
+import random
+
 app = FastAPI()
 
 
@@ -10,5 +12,23 @@ async def root():
     return {"message": "takoyaki🐙"}
 
 @app.get("/items/{item_id}")
+
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+    @app.get("/omikuji")
+def omikuji():
+    omikuji_list = [
+        "大吉",
+        "中吉",
+        "小吉",
+        "吉",
+        "半吉",
+        "末吉",
+        "末小吉",
+        "凶",
+        "小凶",
+        "大凶"
+    ]
+
+    return omikuji_list[random.randrange(10)]
